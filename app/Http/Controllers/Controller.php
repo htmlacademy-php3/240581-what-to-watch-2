5c8a25d402e43c35dba79334c7716e321ed74c3c
vya58 <vya58@yandex.ru>
2023-03-12 15:59:25 +0300

    Ответы без вопросов: исправление

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -6,9 +6,6 @@ use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Routing\Controller as BaseController;
-use Symfony\Component\HttpFoundation\Response;
-use App\Http\Responses\AbstractApiResponse;
-use App\Services\ResponseService;
 
 class Controller extends BaseController
 {
