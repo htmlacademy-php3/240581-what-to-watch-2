3963d02d9551df7e97400b60ed2fef426a428704
vya58 <vya58@yandex.ru>
2023-03-24 20:11:51 +0300

    6.4. Первый юнит-тест: п. 3 тест для CommentModel

--- tests/Unit/CommentTest.php
+++ tests/Unit/CommentTest.php
@@ -2,17 +2,58 @@
 
 namespace Tests\Unit;
 
-use PHPUnit\Framework\TestCase;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Database\Eloquent\Factories\Sequence;
+//use PHPUnit\Framework\TestCase;
+use Database\Seeders\DatabaseSeeder;
+use \App\Models\Comment;
+use \App\Models\Film;
+use \App\Models\User;
+use Tests\TestCase;
 
 class CommentTest extends TestCase
 {
+    use RefreshDatabase;
+
+
     /**
-     * A basic unit test example.
+     * Проверка, что у комментария есть специальное свойство для возврата имени автора и это свойство действительно содержит имя пользователя, который написал данный комментарий.
      *
      * @return void
      */
-    public function test_example()
+    public function test_to_get_comment_author_name(): void
     {
-        $this->assertTrue(true);
+        $film = Film::factory()->create();
+        $user = User::factory()->create();
+
+        $comment = Comment::factory()
+            ->state(new Sequence(
+                fn ($sequence) => ['film_id' => $film, 'user_id' => $user],
+            ))
+            ->create();
+
+        $autor = $comment->user;
+
+        $this->assertEquals($user->name, $autor->name);
+    }
+
+    /**
+     * Проверка, что для анонимных комментариев вместо имени автора выводиться дефолтный текст.
+     *
+     * @return void
+     */
+    public function test_with_anonymous_comment(): void
+    {
+        $film = Film::factory()->create();
+
+        $comment = Comment::factory()
+            ->state(new Sequence(
+                fn ($sequence) => ['film_id' => $film, 'user_id' => null],
+            ))
+            ->create();
+
+        $autor = $comment->user;
+
+        $this->assertEquals('Автор неизвестен', $autor->name);
     }
 }
