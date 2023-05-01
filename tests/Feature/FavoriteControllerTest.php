3490fbfb58af8bbc25417ae573dd37f1c78dbbc1
vya58 <vya58@yandex.ru>
2023-05-01 17:59:11 +0300

    8.5. Финал Выполнил пункт ТЗ: 3.12 Удаление фильма из избранного [DELETE /api/films/{id}/favorite/]

--- tests/Feature/FavoriteControllerTest.php
+++ tests/Feature/FavoriteControllerTest.php
@@ -3,8 +3,6 @@
 namespace Tests\Feature;
 
 use Illuminate\Foundation\Testing\RefreshDatabase;
-use Illuminate\Foundation\Testing\WithFaker;
-use Illuminate\Database\Eloquent\Factories\Sequence;
 use Tests\TestCase;
 use Laravel\Sanctum\Sanctum;
 use \App\Models\Favorite;
